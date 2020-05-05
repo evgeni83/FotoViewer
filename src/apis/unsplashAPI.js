cc4c7e82ae3bcf5367c3a108a757b40d4b869e7c
evgeni83 <evgeni.daniluk@yandex.ru>
2020-05-05 21:11:37 +0300

    Changed url protocol from https to http in unsplashAPI.js

--- src/apis/unsplashAPI.js
+++ src/apis/unsplashAPI.js
@@ -4,7 +4,7 @@ export const unsplash = new Unsplash({
   accessKey: "69a71056cc02e30857197cc9b6512542d933bc3b17e30eb385b6aba920bf5734",
   secret: "351731b0a6ee01c5e04b2c865b630042e52bd9ef4c261baeb752be5f717c8331",
   // callbackUrl: "http://localhost:3000/main"
-  callbackUrl: "https://fotoviewer.herokuapp.com/"
+  callbackUrl: "http://fotoviewer.herokuapp.com/"
 });
 
 export const authenticationUrl = unsplash.auth.getAuthenticationUrl([
