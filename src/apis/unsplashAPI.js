8d559e7d1cdb655aef5337679846a8a77c4d101b
evgeni83 <evgeni.daniluk@yandex.ru>
2020-05-05 20:53:21 +0300

    Changed url for redirect in unsplashAPI.js

--- src/apis/unsplashAPI.js
+++ src/apis/unsplashAPI.js
@@ -3,7 +3,8 @@ import Unsplash from "unsplash-js";
 export const unsplash = new Unsplash({
   accessKey: "69a71056cc02e30857197cc9b6512542d933bc3b17e30eb385b6aba920bf5734",
   secret: "351731b0a6ee01c5e04b2c865b630042e52bd9ef4c261baeb752be5f717c8331",
-  callbackUrl: "http://localhost:3000/main"
+  // callbackUrl: "http://localhost:3000/main"
+  callbackUrl: "https://fotoviewer.herokuapp.com/"
 });
 
 export const authenticationUrl = unsplash.auth.getAuthenticationUrl([
