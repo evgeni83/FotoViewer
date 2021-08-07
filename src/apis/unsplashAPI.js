c7cad468488abf0c25acadffe8310b7a28a6e325
evgeni83 <evgeni.daniluk@yandex.ru>
2021-08-07 15:15:44 +0300

    credz moved to .env

--- src/apis/unsplashAPI.js
+++ src/apis/unsplashAPI.js
@@ -1,17 +1,17 @@
 import Unsplash from "unsplash-js";
 
-export const unsplash = new Unsplash({
-  accessKey: "69a71056cc02e30857197cc9b6512542d933bc3b17e30eb385b6aba920bf5734",
-  secret: "351731b0a6ee01c5e04b2c865b630042e52bd9ef4c261baeb752be5f717c8331",
-  // callbackUrl: "http://localhost:3000/main"
-  callbackUrl: "https://photoviewer.danevgen.ru/main"
-});
+export const unsplash = new Unsplash( {
+    accessKey: process.env.REACT_APP_ACCESS_KEY,
+    secret: process.env.REACT_APP_SECRET,
+    callbackUrl: process.env.REACT_APP_CALLBACK_URL
+    // ""
+} );
 
-export const authenticationUrl = unsplash.auth.getAuthenticationUrl([
-  "public",
-  "write_likes"
-]);
+export const authenticationUrl = unsplash.auth.getAuthenticationUrl( [
+    "public",
+    "write_likes"
+] );
 
 export const redirectToAuthPage = () => {
-  window.location.assign(authenticationUrl);
+    window.location.assign( authenticationUrl );
 };
